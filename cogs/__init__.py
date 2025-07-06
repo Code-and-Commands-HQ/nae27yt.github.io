944fb88f6d7379176614a20ee6e4ea55e258a286
nae27yt <nathanfelix622@gmail.com>
2025-07-06 00:54:08 -0700

    discord

--- cogs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Cogs package for Discord bot
-Contains all command modules and extensions
-"""
-
-# This file makes the cogs directory a Python package
--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Utils package for Discord bot
-Contains utility functions and helper classes
-"""
-
-# This file makes the utils directory a Python package
