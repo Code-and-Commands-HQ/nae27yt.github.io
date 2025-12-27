063a55550a1d7e5f5d6b22301cfbf9dfa7c01e29
nae27yt <nathanfelix622@gmail.com>
2025-12-27 01:03:15 -0700

    ok

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,15 @@
 const firebaseConfig = {
-    apiKey: "YOUR_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA...yourKeyHere...",
+  authDomain: "ghws-11a69.firebaseapp.com",
+  projectId: "ghws-11a69",
+  storageBucket: "ghws-11a69.appspot.com",
+  messagingSenderId: "825268332046",
+  appId: "1:825268332046:web:b4ef681b738b8fec0a36f"
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
+
 
 auth.onAuthStateChanged(user => {
     if (!user) window.location.href = "index.html";
